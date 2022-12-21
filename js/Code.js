1e5b24fc1cdd3a9d9f9ff535ed0c4b5168926a8f
guiseninha <107883094+guiseninha@users.noreply.github.com>
2022-12-21 16:33:42 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VINGADORES";
+const accessCode2 = "HOMEM ARANHA";
+const accessCode3 = "THANOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("A R S E V I D N G O", 100,50)
+    fill("lightblue")
+    text("Dica:Maior saga de herois do cinema", 100,70)
+
+    fill("white")
+    textSize(15)
+    text(" M M H E O   R H N A A A ", 700,150)
+    fill("lightblue")
+    text("Dica: Virou heroi por causa de uma aranha", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("H A N O T S", 100,250)
+    fill("lightblue")
+    text("Dica:Maior vilao dos vingadores", 100,270)
+
+}
